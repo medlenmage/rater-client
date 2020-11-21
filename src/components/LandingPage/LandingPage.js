6da77bdf1f33cdfbf3ebbe84693cc204bac3dc91
Joshua Medlen <joshua246@gmail.com>
2020-11-21 10:20:01 -0600

    set up navbar and auth components

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NavBar } from '../NavBar/NavBar'
+
+export const LandingPage = () => {
+  return (
+    <div className="landing">
+      <NavBar/>
+    </div>
+  )
+}
